96f2e7945991d7b40077acf3fc01bfc75145e605
Abdallah <abdallah.yossf@gmail.com>
2019-12-08 16:16:00 +0200

    Added more input validation and adjusted the structure

--- main/main.c
+++ main/main.c
@@ -18,6 +18,8 @@ void initialize_grid(Game *game){
         for(int j=0; j<grid_length; j++){
             game->grid[i][j].right=  false;
             game->grid[i][j].down = false;
+            game->grid[i][j].left=  false;
+            game->grid[i][j].up=  false;
             printf("%d%d ",game->grid[i][j].right,game->grid[i][j].down);
         }
         printf("\n");
@@ -32,42 +34,34 @@ void print_game_struct_values(Game *game){
 void display_top_10_players(){
 }
 
-void enter_options (Game *game){
+bool enter_options (Game *game){
     printf("Pick a menu number:\n\
            1. Beginner (2 by 2 grid)\n\
            2. Expert (5 by 5 grid)\n");
 
-    char input[10];
-    fgets(input,5,stdin);
-    if(strlen(input) != 2)
-        exit(0);
+    char input[100000];
+    bool invalid_input = 0;
+    do{
+        if(invalid_input)
+            printf("Enter A Valid Input:\n");
+        fgets(input,1000,stdin);
+        invalid_input = (input[0] != '1' && input[0] != '2') || strlen(input) != 2;
+    }while( invalid_input );
 
-    if(input[0] == '1')
-        game->expert = 0;
-
-    else if(input[0] == '2')
-        game->expert = 1;
-
-    else
-        exit(0);
-        // terminate program ??
+            // terminate program ??
     system("@cls||clear");
     printf("Pick a menu number: \n \
            1. Players vs Player\n \
            2. Player vs Computer\n");
 
-    fgets(input,4,stdin);
-    if(strlen(input) != 2)
-        exit(0);
+    invalid_input = 0;
+    do{
+        if(invalid_input)
+            printf("Enter A Valid Input:\n");
+        fgets(input,1000,stdin);
+        invalid_input = (input[0] != '1' && input[0] != '2') || strlen(input) != 2;
+    }while( invalid_input );
 
-    if(input[0] == '1')
-        game->PVP = 1;
-
-    else if(input[0] == '2')
-        game->PVP = 0;
-
-    else
-        exit(0);
         // terminate program
 
     //print_game(game);
@@ -79,22 +73,23 @@ void print_grid(Game *game){
     int grid_length = 4;
     int scale = 3;
 //    Sqaure *grid = game->grid;
-    char full_side[3+1]= "---";
-    char empty_side[3+1]= "   ";
+    char full_side[scale+1], empty_side[scale+1];
+    fill_string(full_side,'-',scale);
+    fill_string(empty_side,' ',scale);
     char in_between_line[(scale+1)*grid_length+10];
 
     /*
         IF the Square of size 3 (scale=2):
 
         main_line               *---*---*---*
-        in_between_line         |   |   |
+        in_between_line         |---|   |
         main_line               *---*
 
 
     */
     initialize_grid(game);
-    game->grid[2][0].right=  1;
-    game->grid[1][3].down = 1;
+    game->grid[1][1].up =  1;
+    game->grid[0][1].left = 1;
 
     printf("\n");
     for(int i=0; i<grid_length; i++){
@@ -103,29 +98,59 @@ void print_grid(Game *game){
             // main_line
             if(j<grid_length-1){
                 printf("+");
-                if(game->grid[i][j].right==1 )
+                if(game->grid[i][j].up==1 )
                     printf("%s",full_side);
                 else
                     printf("%s",empty_side);
             }
             // in_between_line
-            if(game->grid[i][j].down == 1)
+            if(game->grid[i][j].left == 1)
                 in_between_line[(scale+1)*j]='|';
 
         }
         printf("+\n");
         if(i != grid_length-1){
-            for(int k=0; k<scale; k++)
+            for(int k=0; k<scale/3; k++)
                 printf("%s\n",in_between_line);
         }
     }
 
 }
 
+bool in_grid(int point, int grid_length){
+    if(point >= 0 && point < grid_length)
+        return 1;
+
+    return 0;
+}
+
+bool validate_points(int row1, int row2, int col1, int col2, Game *game){
+    int grid_length = game->grid_length;
+    if( ! (in_grid(row1,grid_length) && in_grid(row2,grid_length) && in_grid(col1,grid_length) && in_grid(col2,grid_length)) ){
+        return false;
+    }
+
+    bool adjacent = (row1 == row2 || col1 == col2)
+    && (abs(row1 - row2) == 1 || abs(col1 - col2 == 1)) ;
+    if(! adjacent)
+        return false;
+
+    return 1;
+
+}
+// 1 1 3 2 >> 1,1
+
 void play_game(Game *game){
     int row1,row2,col1,col2;
     for(int i=0; i<10; i++){
-        scanf("%d %d %d %d",&row1,&row2,&col1,&col2);
+
+        bool valid_input = 1;
+        do{
+            if(!valid_input)
+                printf("Enter Valid numbers\n");
+            int num_of_inputs = scanf("%d %d %d %d",&row1,&row2,&col1,&col2);
+            valid_input = num_of_inputs == 4 && validate_points(row1,row2,col1,col2,game);
+        } while (!valid_input);
 
 
     }
@@ -177,8 +202,15 @@ int main()
 //    printf("%d",strlen(arr));
 
     Game game;
-    print_grid(&game);
 
     //play_main_menu(&game);
+//    print_grid(&game);
+    //printf("%d", validate_points(1,1,3,));
+
+     // int row1,row2,col1,col2;
+    // printf("%d\n" ,scanf("%d %d %d %d",&row1,&row2,&col1,&col2));
+    // printf("%d\n", col1);
+
+    play_game(&game);
     return 0;
 }
--- main/structs.h
+++ main/structs.h
@@ -5,11 +5,13 @@ typedef struct{
 
 typedef struct{
     bool up,right,down,left;
+    int player;
 } Square;
 
 typedef struct {
     bool expert;
     bool PVP;
+    int grid_length;
     int player1_points;
     int player2_points;
     Square grid[20][20];
